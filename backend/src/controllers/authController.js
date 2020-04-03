08bf9b1bb80a6a0fd3ffa4de61344c11f1f4d837
Otavio Souza <otavio.silva133@etec.sp.gov.br>
2020-04-03 17:30:47 -0300

    .

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,55 +1,57 @@
 const User = require('../models/user');
 
-module.exports = {
-    login: async (req, res) => {
-        let { email, password } = req.body;
+async function login(req, res) {
+    let { email, password } = req.body;
 
-        try {
-            let newUser = await User.findOne({ email, password })
-            if (!newUser) {
-                return res.send({ message: 'User is not found' })
-            }
-
-            return res.send({
-                message: 'Login sucessfully',
-                token: '123456'
-            })
-
-        } catch (err) {
-            console.log(err)
-            return res.send({ message: 'Error while create user' })
+    try {
+        let newUser = await User.findOne({ email, password })
+        if (!newUser) {
+            return res.send({ message: 'User is not found' })
         }
-    },
 
-    register: async (req, res) => {
-        let { name, age, email, password } = req.body;
+        return res.send({
+            message: 'Login sucessfully',
+            token: '123456'
+        })
 
-        try {
-            let newUser = await User.findOne({ email });
-
-            if (newUser) {
-                return res.send({
-                    user: newUser,
-                    message: 'User already exists'
-                });
-            }
-
-            newUser = await User.create({
-                name,
-                age,
-                email,
-                password
-            });
-
-            newUser.password = undefined;
-
-            res.send({
-                user: newUser
-            });
-
-        } catch (err) {
-            console.log(err)
-            return res.send({ message: 'Error while create user' })
-        }
+    } catch (err) {
+        console.log(err)
+        return res.send({ message: 'Error while create user' })
     }
+}
+
+async function register(req, res) {
+    let { name, age, email, password } = req.body;
+
+    try {
+        let newUser = await User.findOne({ email });
+
+        if (newUser) {
+            return res.send({
+                user: newUser,
+                message: 'User already exists'
+            });
+        }
+
+        newUser = await User.create({
+            name,
+            age,
+            email,
+            password
+        });
+
+        newUser.password = undefined;
+
+        res.send({
+            user: newUser
+        });
+
+    } catch (err) {
+        console.log(err)
+        return res.send({ message: 'Error while create user' })
+    }
+}
+
+module.exports = {
+    login, register
 };
--- backend/src/controllers/postsController.js
+++ backend/src/controllers/postsController.js
@@ -1,11 +1,13 @@
+async function createPost(req, res) {
+    const { filename } = req.file;
+    res.status(200).send({
+        message: 'Uploaded with sucess',
+        data: {
+            url: `${process.env.APP_URL}/files/${filename}`
+        }
+    });
+}
+
 module.exports = {
-    createPost: (req, res) => {
-        const { filename } = req.file;
-        res.status(200).send({
-            message: 'Uploaded with sucess',
-            data: {
-                url: `${process.env.APP_URL}/files/${filename}`
-            }
-        });
-    }
+    createPost
 };
